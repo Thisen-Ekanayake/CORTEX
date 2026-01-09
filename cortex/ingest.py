1b997507945d19d7fe9a0e1f3ed2f559905ab9ea
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2026-01-09 11:48:00 +0530

    implement doc ingestion

---
+++ cortex/ingest.py
@@ -0,0 +1,68 @@
+import os
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    TextLoader,
+    Docx2txtLoader
+)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+from cortex.embeddings import get_embeddings
+
+
+DATA_DIR = "data/documents"
+PERSIST_DIR = "chroma_db"
+
+
+def load_documents():
+    documents = []
+
+    for filename in os.listdir(DATA_DIR):
+        path = os.path.join(DATA_DIR, filename)
+
+        if filename.endswith(".pdf"):
+            loader = PyPDFLoader(path)
+        elif filename.endswith(".txt"):
+            loader = TextLoader(path)
+        elif filename.endswith(".docx"):
+            loader = Docx2txtLoader(path)
+        else:
+            continue
+
+        documents.extend(loader.load())
+
+    return documents
+
+
+def ingest():
+    print("Loading documents...")
+    documents = load_documents()
+
+    if not documents:
+        print("No documents found.")
+        return
+
+    print(f"Loaded {len(documents)} documents")
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=100
+    )
+
+    chunks = splitter.split_documents(documents)
+    print(f"🔹 Split into {len(chunks)} chunks")
+
+    embeddings = get_embeddings()
+
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=PERSIST_DIR
+    )
+
+    vectorstore.persist()
+    print("Chroma DB persisted successfully")
+
+
+if __name__ == "__main__":
+    ingest()
